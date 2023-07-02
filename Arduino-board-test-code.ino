14d441f10ef9c0b8843b50df62d789de942904cb
Jonas <36644388+QC20@users.noreply.github.com>
2023-07-02 20:23:10 +0200

    Add files via upload

---
+++ Arduino-board-test-code.ino
@@ -0,0 +1,220 @@
+// Blinking LED
+// Small piece of code that will look non-random.
+// You can upload it to your arduino/clone boards to check if they work properly.
+
+// Pin number for the LED
+const int ledPin = 13;
+
+void setup() {
+  // Initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(1000);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(1000);
+    // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(500);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(500);
+    // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(250);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(250);
+    // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(125);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(125);
+      // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(750);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(750);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(1500);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(1500);
+      // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(250);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(250);
+      // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(100);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(100);
+      // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+  // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(200);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(200);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+          // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(200);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(200);        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(200);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(200);
+        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(50);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(50);
+  // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(750);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(750);
+          // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(200);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(200);        // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(200);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(200);
+          // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(750);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(750);
+          // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(500);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(500);
+  // Turn on the LED
+  digitalWrite(ledPin, HIGH);
+  // Delay for 1 second
+  delay(500);
+  // Turn off the LED
+  digitalWrite(ledPin, LOW);
+  // Delay for 1 second
+  delay(500);
+}
